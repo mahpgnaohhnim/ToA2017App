875cb0035c8bb7e7dc66c268ff5c6d5b5e266f8c
Pham Minh Hoang Henry <minhhoangpham@live.de>
2017-09-04 01:24:47 +0200

    initial commit

---
+++ app/src/main/java/com/mahpgnaohhnim/aco/kassensystem/toa2017/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mahpgnaohhnim.aco.kassensystem.toa2017;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by henry on 04.09.17.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
