922ae0319a59b538c6b111a08d93b4a0212d097c
Pham Minh Hoang Henry <minhhoangpham@live.de>
2017-09-07 02:25:25 +0200

    finished Skeletton

---
+++ app/src/main/java/com/mahpgnaohhnim/aco/kassensystem/toa2017/ItemLinearLayout.java
@@ -0,0 +1,99 @@
+package com.mahpgnaohhnim.aco.kassensystem.toa2017;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by henry on 06.09.17.
+ */
+public class ItemLinearLayout extends LinearLayout {
+    String itemName;
+    TextView nameLabel, quantityLabel;
+    int quantity;
+    Float sellPrice;
+    final Button subBtn, addBtn;
+    MainActivity mainActivity;
+    LinearLayout rightContainer;
+
+
+    public ItemLinearLayout(final Context context, String name, final Float price){
+        super(context);
+        mainActivity = (MainActivity) context;
+
+        LinearLayout.LayoutParams myLayoutParam = new LinearLayout.LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT);
+
+        this.setLayoutParams(myLayoutParam);
+
+        this.itemName = name;
+        this.sellPrice = price;
+        this.quantity = 0;
+
+        LinearLayout.LayoutParams containerParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT,1f);
+        LinearLayout.LayoutParams childParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 1f);
+
+        rightContainer = new LinearLayout(context);
+        rightContainer.setLayoutParams(containerParams);
+        rightContainer.setOrientation(HORIZONTAL);
+        nameLabel = new TextView(context);
+        nameLabel.setLayoutParams(childParams);
+        nameLabel.setText(this.itemName);
+        nameLabel.setGravity(Gravity.CENTER_VERTICAL| Gravity.LEFT);
+
+        quantityLabel = new TextView(context);
+        addBtn = new Button(context);
+        subBtn = new Button(context);
+
+        quantityLabel.setLayoutParams(childParams);
+        quantityLabel.setTypeface(null, Typeface.BOLD);
+        quantityLabel.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+        quantityLabel.setText("0");
+
+        addBtn.setLayoutParams(childParams);
+        addBtn.setText("+");
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                quantity += 1;
+                updateLabels();
+            }
+        });
+
+        subBtn.setLayoutParams(childParams);
+        subBtn.setText("-");
+        subBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(quantity>0) {
+                    quantity -= 1;
+                    updateLabels();
+                }
+            }
+        });
+
+        this.addView(nameLabel);
+        this.addView(rightContainer);
+
+        rightContainer.addView(subBtn);
+        rightContainer.addView(quantityLabel);
+        rightContainer.addView(addBtn);
+    }
+
+    private void updateLabels(){
+        quantityLabel.setText(Integer.toString(this.quantity));
+        mainActivity.updateTotalSum();
+    }
+
+    public void resetStats(){
+        this.quantity = 0;
+        updateLabels();
+    }
+
+}
--- app/src/main/java/com/mahpgnaohhnim/aco/kassensystem/toa2017/MainActivity.java
+++ app/src/main/java/com/mahpgnaohhnim/aco/kassensystem/toa2017/MainActivity.java
@@ -3,6 +3,13 @@ package com.mahpgnaohhnim.aco.kassensystem.toa2017;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by henry on 04.09.17.
@@ -10,9 +17,46 @@ import android.support.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
+    LinearLayout contentContainer;
+    Float totalSum;
+    ItemLinearLayout adultItem, childItem;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //request Fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        contentContainer = (LinearLayout) findViewById(R.id.contentContainer);
+        adultItem = new ItemLinearLayout(this, "Erwachsener", 4.5f);
+        childItem = new ItemLinearLayout(this, "Kind", 1.5f);
+        LinearLayout totalSum = (LinearLayout) View.inflate(this,R.layout.summary_value_linearlayout,null);
+
+        contentContainer.addView(adultItem);
+        contentContainer.addView(childItem);
+        contentContainer.addView(totalSum);
     }
+
+    public void updateTotalSum(){
+        calcTotalSum();
+        TextView totalSumLabel = (TextView) findViewById(R.id.totalSum);
+        if(totalSum == 0){
+            totalSumLabel.setText("0€");
+        }else {
+            totalSumLabel.setText(Float.toString(totalSum) + "€");
+        }
+    }
+
+    private void calcTotalSum(){
+        Float adultSum = adultItem.sellPrice * adultItem.quantity;
+        Float childSum = childItem.sellPrice * childItem.quantity;
+        totalSum = adultSum + childSum;
+    }
+
+
+
 }
